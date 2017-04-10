kima-ventures/reporting

commit 4efeb92f611c771c2f99216d1b3473f83ef1a922

Fix issues with people having accents in their name

app/models/message.py
@@ -142,5 +142,5 @@ class Message(models.Model):
         server = smtplib.SMTP(settings.EMAIL_HOST, settings.EMAIL_PORT)
         server.starttls()
         server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
-        server.sendmail(self.mail_from, mail_to, e.as_string())
+        server.sendmail(self.mail_from.encode('utf-8'), mail_to.encode('utf-8'), e.as_string())
         server.quit()

commit 59cb729000aeb3509c2c78d50a1d8eaa5158149f

Fix a regression introduced in 4efeb92f611c771c2f99216d1b3473f83ef1a922 where e-mails were not relaying anymore

app/models/message.py
@@ -142,5 +142,5 @@ class Message(models.Model):
         server = smtplib.SMTP(settings.EMAIL_HOST, settings.EMAIL_PORT)
         server.starttls()
         server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
-        server.sendmail(self.mail_from.encode('utf-8'), mail_to.encode('utf-8'), e.as_string())
+        server.sendmail(self.mail_from.encode('utf-8'), map(lambda x : x.encode('utf-8'), mail_to), e.as_string())
         server.quit()