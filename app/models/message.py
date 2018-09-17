7c31b337f5616ee587cd0872d0d76d4a829efdf3
Alexis Robert <alexis.robert@gmail.com>
2018-09-17 15:18:20 +0200

    Don't relay the e-mail if there's no recipient for the relay (Python's smtplib doesn't like that)

--- app/models/message.py
+++ app/models/message.py
@@ -140,6 +140,10 @@ class Message(models.Model):
             if u.userprofile.relay_email and u.email.lower().strip() != self.mail_from.lower().strip():
                 mail_to.append(u.email)
 
+        # Don't continue if there's no recipient (smtplib doesn't like that)
+        if len(mail_to) == 0:
+            return
+
         # Edit the subject
         prepend_with = u"[{0}-Reporting] [{1}] ".format(os.getenv("FUND_NAME", "Kima"),
                                                         self.mailbox.startup.name)
